faf3eeb83560104a16270392cb951aa0fc807a85
mrSam <samakpan817@gmail.com>
2024-01-24 18:22:56 +0100

    Completed parrot lab

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,10 @@
-def parrot():
-    pass
+def parrot(sound_S ="Squawk!"):
+    print(sound_S)
+    if sound_S == "":
+        print('no squark')
+        return sound_S
+  
+    return sound_S
+parrot_sound = parrot()
+# print(parrot_sound)
+pass
